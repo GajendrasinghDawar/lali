3885e289cc9ac7ee541956bdb36ea5636a57d32b
Gajendrasingh Dawar <gajendrasinghdawar@gmail.com>
2026-09-02 10:46:10 +0530

    chore: remove accidental files

--- scratch.cjs
+++
@@ -1,3 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const db = new DatabaseSync(':memory:');
-console.log(db.prepare("SELECT '2026-09-02T00:00:00.000Z' > datetime('now') as res").get());
